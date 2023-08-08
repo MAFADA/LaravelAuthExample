245efa0ea5f20817902f11cc8f327eff528f8a31
M. AFADA NUR SAIVA SYAHIRA <afadanursaiva2001@gmail.com>
2023-08-08 10:40:52 +0700

    add Register with OTP Verif Twilio

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/verify', function () {
+    return view('auth.verify');
+})->name('verify');
+Route::post('/verify', [App\Http\Controllers\Auth\RegisterController::class, 'verify'])->name('verify');
